5beef9151efa0625f4ecc122e1733636d0b332fb
GuilhermeShinti <guilhermeshinti@gmail.com>
2022-01-17 03:24:38 -0300

    fix course page

--- src/pages/Course/index.tsx
+++ src/pages/Course/index.tsx
@@ -1,13 +1,16 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import { Container } from "./styles";
 
 export function Course() {
+    const location = useLocation();
+    const currentPage = location.pathname.split('/').reverse()[0];
+    
     return (
         <>
             <Container>
                 <header>
-                    <Link to="modulos"><div className="active">Módulos</div></Link>
-                    <Link to="aulas"><div>Aulas</div></Link>
+                    <Link to="modulos"><div className={`${currentPage === "modulos" ? "active" : ""}`}>Módulos</div></Link>
+                    <Link to="aulas"><div className={`${currentPage === "aulas" ? "active" : ""}`}>Aulas</div></Link>
                 </header>
             </Container>
             <Outlet />
